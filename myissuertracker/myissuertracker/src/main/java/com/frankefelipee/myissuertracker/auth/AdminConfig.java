2a1ea96db273ff30f4ce0277cbbff412ce891956
FrankeFelipe5 <frankefelipee@gmail.com>
2024-12-09 00:03:58 -0300

    Update gitignore

--- myissuertracker/myissuertracker/src/main/java/com/frankefelipee/myissuertracker/auth/AdminConfig.java
+++ myissuertracker/myissuertracker/src/main/java/com/frankefelipee/myissuertracker/auth/AdminConfig.java
@@ -1,10 +1,15 @@
 package com.frankefelipee.myissuertracker.auth;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.frankefelipee.myissuertracker.user.User;
 import com.frankefelipee.myissuertracker.user.UserRepository;
+import com.frankefelipee.myissuertracker.user_role.UserRole;
 import com.frankefelipee.myissuertracker.user_role.UserRoleRepository;
 
 @Configuration
@@ -12,20 +17,52 @@ public class AdminConfig implements CommandLineRunner {
 
     private final UserRoleRepository userRoleRepository;
     private final UserRepository userRepository;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public AdminConfig(UserRoleRepository userRoleRepository, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    @Value("${server.admin.username}")
+    private final String adminUserName;
+
+    @Value("${server.admin.password}")
+    private final String adminPassword;
+
+    public AdminConfig(
+            UserRoleRepository userRoleRepository,
+            UserRepository userRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder,
+            String adminUserName,
+            String adminPassword
+    ) {
 
         this.userRoleRepository = userRoleRepository;
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.adminUserName = adminUserName;
+        this.adminPassword = adminPassword;
 
     }
 
     @Override
     public void run(String... args) {
 
-        // TODO Auto-generated method stub
+        var admin = userRepository.findByName(adminUserName);
+        var adminRole = userRoleRepository.findByName(UserRole.Values.ADMIN.name());
+
+        if (adminRole.isEmpty()) {
+            var basicRole = new UserRole();
+            basicRole.setName("BASIC");
+            userRoleRepository.save(basicRole);
+            var newAdminRole = new UserRole();
+            newAdminRole.setId(2L);
+            newAdminRole.setName("ADMIN");
+        }
+
+        if (admin.isEmpty()) {
+            var user = new User();
+            user.setName(adminUserName);
+            user.setPassword(bCryptPasswordEncoder.encode(adminPassword));
+            user.setRoles(Set.of(adminRole.get()));
+            userRepository.save(user);
+        }
 
     }
 
--- myissuertracker/myissuertracker/src/main/java/com/frankefelipee/myissuertracker/user_role/UserRoleRepository.java
+++ myissuertracker/myissuertracker/src/main/java/com/frankefelipee/myissuertracker/user_role/UserRoleRepository.java
@@ -2,8 +2,12 @@ package com.frankefelipee.myissuertracker.user_role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+
+    Optional<UserRole> findByName(String name);
 
 }
