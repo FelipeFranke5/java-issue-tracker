a1d557be3fb4b5adfdda8a098d934d9f00cb57ea
FrankeFelipe5 <frankefelipee@gmail.com>
2024-12-10 10:59:50 -0300

    Update getToken to use AuthService

--- myissuertracker/myissuertracker/src/main/java/com/frankefelipee/myissuertracker/auth/AuthController.java
+++ myissuertracker/myissuertracker/src/main/java/com/frankefelipee/myissuertracker/auth/AuthController.java
@@ -1,19 +1,12 @@
 package com.frankefelipee.myissuertracker.auth;
 
-import java.time.Instant;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.frankefelipee.myissuertracker.user.UserRepository;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -24,31 +17,22 @@ import lombok.AllArgsConstructor;
 public class AuthController {
 
     private final JwtEncoder jwtEncoder;
-    private final UserRepository userRepository;
+    private final AuthService authService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping("/get_token")
     public ResponseEntity<AuthResponse> getToken(@Valid @RequestBody AuthRequest authRequest) {
 
-        var user = userRepository.findByName(authRequest.name());
+        String token = authService.getValidToken(authRequest, jwtEncoder, bCryptPasswordEncoder);
 
-        if ((user.isEmpty()) || (!user.get().isLoginOk(authRequest, bCryptPasswordEncoder))) {
-            throw new BadCredentialsException("User or Password is incorrect");
+        if (token == null) {
+
+            return ResponseEntity.status(401).build();
+
         }
 
-        var now = Instant.now();
-        var expiresIn = 500L;
-
-        var claims = JwtClaimsSet.builder()
-                .issuer("myissuertracker")
-                .subject(user.get().getId().toString())
-                .expiresAt(now.plusSeconds(expiresIn))
-                .issuedAt(now)
-                .build();
-
-        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-
-        return ResponseEntity.ok(new AuthResponse(jwtValue, expiresIn));
+        AuthResponse response = new AuthResponse(token, 500L);
+        return ResponseEntity.ok(response);
 
     }
 
