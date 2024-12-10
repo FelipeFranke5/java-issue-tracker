8fc1eea2645709cf3b12bbe83cc59fa393ec9db3
FrankeFelipe5 <frankefelipee@gmail.com>
2024-12-10 11:01:38 -0300

    Add blank space to main function

--- myissuertracker/myissuertracker/src/main/java/com/frankefelipee/myissuertracker/MyissuertrackerApplication.java
+++ myissuertracker/myissuertracker/src/main/java/com/frankefelipee/myissuertracker/MyissuertrackerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyissuertrackerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MyissuertrackerApplication.class, args);
+
 	}
 
 }
